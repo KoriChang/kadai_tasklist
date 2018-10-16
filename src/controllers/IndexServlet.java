d7bbac65bf0d100de0059528f55fc2fdbea45fad
LAPTOP-2O5MOQK1\kori <kowen.chou@gmail.com>
2018-10-16 21:33:35 +0900

    fix

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -17,7 +17,7 @@ import utils.DBUtil;
 /**
  * Servlet implementation class IndexServlet
  */
-@WebServlet( "/index")
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -35,17 +35,28 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
         List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
-                                   .getResultList();
+                                    .setFirstResult(15 * (page - 1))
+                                    .setMaxResults(15)
+                                    .getResultList();
+
+        long tasks_count = (long)em.createNamedQuery("getTasksCount", Long.class)
+                                         .getSingleResult();
 
         em.close();
 
         request.setAttribute("tasks", tasks);
+        request.setAttribute("tasks_count", tasks_count);
+        request.setAttribute("page", page);
         if(request.getSession().getAttribute("flush") != null) {
             request.setAttribute("flush", request.getSession().getAttribute("flush"));
             request.getSession().removeAttribute("flush");
         }
-
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
         rd.forward(request, response);
 	}
